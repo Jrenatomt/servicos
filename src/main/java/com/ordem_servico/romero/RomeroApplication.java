eef6e554c568ba6f68c007b8be02bf5f107b85af
Jrenatomt <jrenatomt@gmail.com>
2024-08-12 20:36:56 -0300

    config de projeto

---
+++ src/main/java/com/ordem_servico/romero/RomeroApplication.java
@@ -0,0 +1,13 @@
+package com.ordem_servico.romero;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RomeroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RomeroApplication.class, args);
+	}
+
+}
